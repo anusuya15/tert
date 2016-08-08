fa0429799df18f40852475f2e828199d772747dd
anusuya15 <anusuyaindhu5@gmail.com>
2016-08-08 02:50:52 -0700

    sd
    sd

---
+++ c1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class unique_number {
+public static void main(String[] args) {
+	Scanner in =new Scanner(System.in);
+	System.out.println("enter the length of array");
+	int l=in.nextInt();
+	int arr[]=new int[l];
+	System.out.println("enter the elements");
+	for(int l=0;l<n;l++)
+	{
+	a[l]=in.nextInt();	
+	}
+	for(int j=0;j<a.length;j++)
+	{
+		if(a[j]==j)
+		{
+			System.out.println(j);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	
+}
+}
+
+		
+
+	}
+
+	
+	 
+	
+	    
